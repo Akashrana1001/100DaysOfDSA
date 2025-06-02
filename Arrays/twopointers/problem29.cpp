1294261d6212afcf9e0f1b2aadf68dbd72f705cb
akash chauhan <sandeepakash537@gmail.com>
2025-06-02 12:07:14 +0530

    removed error and bugs from problems.cpp

--- Arrays/twopointers/problem29.cpp
+++ Arrays/twopointers/problem29.cpp
@@ -1,42 +1,28 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-int main() {
-    // Input the size of the array
+int main(){
     int a;
-    cin >> a;
-    
-    // Create a vector of size a
-    vector<int> v(a);
-    
-    // Input the elements of the array
-    for (int i = 0; i < a; i++) {
-        cin >> v[i];
+    cin>>a;
+
+    vector<int>v(a);
+    for(int i=0;i<a;i++){
+        cin>>v[i];
     }
 
-    // Edge case: if the array is empty
-    if (a == 0) {
-        cout << 0;
-        return 0;
-    }
-
-    // Two pointers approach
-    int slow = 0;
-    for (int fast = 1; fast < a; fast++) {
-        if (v[slow] != v[fast]) {
+    int slow  = 0;
+    for(int fast=1;fast<a;fast++){
+        if(v[slow]!=v[fast]){
             slow++;
-            v[slow] = v[fast];
+            v[slow]=v[fast];
         }
+        
     }
 
-    // Output the length of the array after removing duplicates
-    cout << slow + 1 << endl;
+    cout<<slow+1<<" is the length of new array after removing the duplicates and the elements are ";
 
-    // Optional: Print the modified array (for clarity)
     for (int i = 0; i <= slow; i++) {
         cout << v[i] << " ";
     }
-
     return 0;
 }
