34349d52a95bcebf9131e2f530e38764412b0769
akash chauhan <sandeepakash537@gmail.com>
2025-06-04 09:33:23 +0530

    solved a error

--- Arrays/twopointers/problem33.cpp
+++ Arrays/twopointers/problem33.cpp
@@ -6,23 +6,25 @@
 using namespace std;
 
 // Function to sort an array of 0s, 1s and 2s
-void sort012(vector<int> &arr) {
-    int n = arr.size();
-    int lo = 0;
-    int hi = n - 1;
-    int mid = 0;
 
-    // Iterate till all the elements
-    // are sorted
-    while (mid <= hi) {
-        if (arr[mid] == 0)
-            swap(arr[lo++], arr[mid++]);
-        else if (arr[mid] == 1)
-            mid++;
-        else
-            swap(arr[mid], arr[hi--]);
+void sort012(vector<int>&arr){
+    int mid=0;
+    int low=0;
+    int high=arr.size()-1;
+  while(mid <= high){
+    if(arr[mid] == 0){
+        swap(arr[mid++], arr[low++]);
+    }
+    else if(arr[mid] == 1){
+        mid++;
+    }
+    else { // arr[mid] == 2
+        swap(arr[mid], arr[high--]);
     }
 }
+
+}
+
 
 int main() {
     vector<int> arr = { 0, 1, 2, 0, 1, 2 };
