2b9a502196ef5df8bb05db509ac4e53a5eef20b2
akash chauhan <sandeepakash537@gmail.com>
2025-06-07 18:09:00 +0530

    added questions in the maps and setsw

--- Arrays/maps and sets/problem37.cpp
+++ Arrays/maps and sets/problem37.cpp
@@ -1,20 +1,32 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
-#include <vector>
 #include <unordered_map>
+#include <sstream>
 using namespace std;
-
-
-
 int main() {
-    int a;
-    cin >> a;
-    vector<int> arr(a);
-
-    for (int i = 0; i < a; i++) {
-        cin >> arr[i];
+    string name;
+    getline(cin, name);    
+    
+    for(char& c:name){
+        if(ispunct(c)){
+            c=' ';
+        }
     }
-
-   
+    
+    istringstream iss(name);
+    string word;
+    unordered_map<string,int> wordcount;
+    
+    while(iss>>word){
+        wordcount[word]++;
+    }
+    
+    for(const auto& it: wordcount)
+    {
+        if(it.second==1){
+            cout<<it.first<<" ";
+        }
+    }
 
     return 0;
 }
