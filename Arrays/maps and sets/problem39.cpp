bff7797f636d9ef3b1220ddda09f1a2ac2661185
akash chauhan <sandeepakash537@gmail.com>
2025-06-07 18:22:19 +0530

    added questions in the maps and setsw

--- Arrays/maps and sets/problem39.cpp
+++ Arrays/maps and sets/problem39.cpp
@@ -6,13 +6,11 @@ const int MAX_CHAR = 26;
 char nonRep(const string &s) {
     vector<int> freq(MAX_CHAR, 0);
 
-    // Traditional loop to count frequencies
     for (int i = 0; i < s.length(); i++) {
         char c = s[i];
         freq[c - 'a']++;
     }
 
-    // Traditional loop to find the first non-repeating character
     for (int i = 0; i < s.length(); i++) {
         char c = s[i];
         if (freq[c - 'a'] == 1) {
