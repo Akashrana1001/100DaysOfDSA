420ea529d3f1df767a79a2d0eef7e48fbd2a8a2b
akash chauhan <sandeepakash537@gmail.com>
2025-06-20 13:28:50 +0530

    Added question in two pointers approach

--- Arrays/twopointers/problem35.cpp
+++ Arrays/twopointers/problem35.cpp
@@ -2,14 +2,33 @@
 #include <vector>
 using namespace std;
 
-
+// trapped rain water in the containers
 
 
 int main() {
-    vector<int> arr = { 11,15,6,8,9,10 };
-    int n = arr.size();
+    vector<int> height = { 11,15,6,8,9,10 };
 
-    
+      int left = 0, right = height.size() - 1;
+    int leftMax = 0, rightMax = 0;
+    int trapped = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            // We are on the lower side
+            if (height[left] >= leftMax)
+                leftMax = height[left];
+            else
+                trapped += (leftMax - height[left]);
+            left++;
+        } else {
+            if (height[right] >= rightMax)
+                rightMax = height[right];
+            else
+                trapped += (rightMax - height[right]);
+            right--;
+        }
+    }
+    cout<<trapped;
 
 
     return 0;
