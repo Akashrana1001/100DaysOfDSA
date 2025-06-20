cdbf6e1b40c2c60f88b909531ca652ee77603f1c
akash chauhan <sandeepakash537@gmail.com>
2025-06-20 19:24:58 +0530

    Added question in two pointers approach

---
+++ Arrays/maps and sets/problem54.cpp
@@ -0,0 +1,16 @@
+//good pairs
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+     unordered_map<int, int> freq;
+        int count = 0;
+
+        for (int num : nums) {
+            // Before adding this occurrence, we can already form "freq[num]" pairs with previous ones
+            count += freq[num];
+            freq[num]++;
+        }
+
+        return count;
+    }
+};
